f167ad183d298d4b038dc6597061e50a9d7b61be
Tony Baines <tony.baines.1@gmail.com>
2021-12-01 14:52:44 +0000

    Day 1 with official input

--- src/main/kotlin/tonyb/day1/SonarSweep.kt
+++ src/main/kotlin/tonyb/day1/SonarSweep.kt
@@ -1,8 +1,18 @@
 package tonyb.day1
 
+import java.nio.file.Files
 import java.util.Objects.nonNull
+import kotlin.io.path.Path
 
 fun countDepthIncreases(depths: Sequence<Int?>): Int =
     sequenceOf(null).plus(depths).zip(depths) // Pairs of adjacent readings
         .filter { (a,b) -> nonNull(a) &&  a!! < b!! } // Find the increases
         .count()
+
+fun main() {
+    val depths = Files.lines(Path("src/test/resources/day1/puzzle_input"))
+        .map { it.toInt() }
+        .toList()
+
+    println(countDepthIncreases(depths.asSequence()))
+}
