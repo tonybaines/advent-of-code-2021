2bf448954160bba74b83f8913cd1fda8894b475a
Tony Baines <tony.baines.1@gmail.com>
2021-12-22 11:57:33 +0000

    Day2 refactor

--- src/main/kotlin/tonyb/sonarsweep/Propulsion.kt
+++ src/main/kotlin/tonyb/sonarsweep/Propulsion.kt
@@ -1,9 +1,31 @@
 package tonyb.sonarsweep
 
+import tonyb.sonarsweep.Direction.*
 import java.nio.file.Files
 import kotlin.io.path.Path
 
 
+fun propulsion(movements: List<String>): Position = propulsion(movements.map { Movement.fromString(it) })
+
+
+@JvmName("propulsion_internal")
+private fun propulsion(movements: List<Movement>): Position = movements.fold(State.ZERO) { acc, move ->
+    when (move.direction) {
+        forward -> acc.copy(
+            position = acc.position
+                .forward(move.amount)
+                .down(acc.aim * move.amount)
+        )
+        up -> acc.copy(aim = acc.aim - move.amount)
+        down -> acc.copy(aim = acc.aim + move.amount)
+    }
+}.position
+
+private data class State(val aim: Int, val position: Position) {
+    companion object {
+        val ZERO = State(0, Position.ZERO)
+    }
+}
 @JvmInline
 value class Horizontal(val value: Int) {
     operator fun plus(amount: Int): Horizontal = Horizontal(value + amount)
@@ -17,6 +39,7 @@ data class Position(val depth: Depth, val horizontal: Horizontal) {
     fun forward(amount: Int): Position = this.copy(horizontal = this.horizontal + amount)
     fun down(amount: Int): Position = this.copy(depth = this.depth + amount)
     fun up(amount: Int): Position = this.copy(depth = this.depth - amount) // Just assume depth will never be negative!
+
     fun multiplied(): Int = depth.value * horizontal.value
 
     companion object {
@@ -32,30 +55,10 @@ data class Movement(val direction: Direction, val amount: Int) {
     companion object {
         fun fromString(s: String): Movement {
             val (direction, distance) = s.split(" ")
-            return Movement(Direction.valueOf(direction), distance.toInt())
+            return Movement(valueOf(direction), distance.toInt())
         }
     }
 }
-
-@JvmName("propulsion_by_string")
-fun propulsion(movements: List<String>): Position = propulsion(movements.map { Movement.fromString(it) })
-
-private data class State(val aim: Int, val position: Position) {
-    companion object {
-        val ZERO = State(0, Position.ZERO)
-    }
-}
-
-fun propulsion(movements: List<Movement>): Position = movements.fold(State.ZERO) { acc, move ->
-    when (move.direction) {
-        Direction.forward -> acc.copy(position = acc.position
-            .forward(move.amount)
-            .down(acc.aim * move.amount)
-        )
-        Direction.up -> acc.copy(aim = acc.aim - move.amount)
-        Direction.down -> acc.copy(aim = acc.aim + move.amount)
-    }
-}.position
 
 fun main() {
     val movements = Files.lines(Path("src/test/resources/day2/puzzle_input"))
